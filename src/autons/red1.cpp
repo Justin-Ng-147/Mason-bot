a5fd082c7d6b10b3fb75b09a8d3cc8e7a77e3a54
Justin <justin.ng147@gmail.com>
2024-10-15 17:19:59 -0700

    done most of skills still need to do work

--- src/autons/red1.cpp
+++ src/autons/red1.cpp
@@ -60,29 +60,29 @@ void red1(){
 
     //get new mogo
     mogo.set_value(false);
-    chassis.turnToPoint(32,17,4000,{.forwards=false});
+    chassis.turnToPoint(32,16,4000,{.forwards=false});
     start_pose = chassis.getPose();
-    chassis.moveToPoint(32,17,4000,{.forwards=false,.minSpeed=60,.earlyExitRange=40});
-    chassis.moveToPoint(32,17,4000,{.forwards=false,.maxSpeed=40});
+    chassis.moveToPoint(32,16,4000,{.forwards=false,.minSpeed=60,.earlyExitRange=40});
+    chassis.moveToPoint(32,16,4000,{.forwards=false,.maxSpeed=40});
     while(start_pose.distance(chassis.getPose()) < 78) pros::delay(10);
     mogo.set_value(true);
 
     //get 1st ring
-    chassis.turnToPoint(30,44,4000);
+    chassis.turnToPoint(28,44,4000);
     intake.move(127);
-    chassis.moveToPoint(30,44,4000,{},false);
+    chassis.moveToPoint(28,44,4000,{},false);
     intake.move(0);
 
     //get 2nd ring
-    chassis.turnToPoint(53,44,4000);
+    chassis.turnToPoint(48,44,4000);
     intake.move(127);
-    chassis.moveToPoint(53,44,4000,{},false);
+    chassis.moveToPoint(48,44,4000,{},false);
     intake.move(0);
     
     //get 3rd ring
-    chassis.turnToPoint(65,69,4000);
+    chassis.turnToPoint(64,69,4000);
     intake.move(127);
-    chassis.moveToPoint(65,69,4000,{},false);
+    chassis.moveToPoint(64,69,4000,{},false);
     intake.move(0);
 
     //get 4th and 5th ring
@@ -95,28 +95,28 @@ void red1(){
 
     //get 6th ring
     intake.move(127);
-    chassis.moveToPose(66,25,-12,4000,{.lead=.8},false);
+    chassis.moveToPose(64,25,-15,4000,{.lead=.5},false);
     intake.move(0);
 
     //score mogo
     chassis.moveDistance(15,1000,{.forwards=false},false);
     mogo.set_value(false);
     pros::delay(500);
-    chassis.moveDistance(15,4000);
+    chassis.moveDistance(10,4000);
 
     // -20 87
     //get 1st ring
     chassis.turnToPoint(-20,87,4000);
     intake.move(127);
-    chassis.moveToPoint(-20,87,4000);
+    chassis.moveToPoint(-20,87,4000,{},false);
     intake.move(0);
 
     //12 120
     //get mogo
-    chassis.turnToPoint(12,120,2000,{.forwards=false});
+    chassis.turnToPoint(12,125,2000,{.forwards=false});
     start_pose = chassis.getPose();
-    chassis.moveToPoint(12,120,4000,{.forwards=false,.maxSpeed=40});
-    while(start_pose.distance(chassis.getPose()) < 30) pros::delay(10);
+    chassis.moveToPoint(12,125,4000,{.forwards=false,.maxSpeed=45});
+    while(start_pose.distance(chassis.getPose()) < 33) pros::delay(10);
     mogo.set_value(true);
 
     //-20,87
@@ -131,6 +131,38 @@ void red1(){
     chassis.turnToPoint(-42,93,4000);
     intake.move(127);
     chassis.moveToPoint(-42,93,4000,{},false);
+    
+
+    //get 4th ring
+    // 180 4 124
+    chassis.moveToPoint(0,123,4000,{.forwards=false},false);
     intake.move(0);
+    // 34 89 
+    chassis.turnToPoint(34,89,4000);
+    intake.move(127);
+    chassis.moveToPoint(34,89,4000,{},false);
+    intake.move(0);
+
+    //get 5th ring
+    // 56 89
+    chassis.turnToPoint(56,90,4000);
+    intake.move(127);
+    chassis.moveToPoint(56,90,4000,{},false);
+    intake.move(0);
+
+    chassis.turnToPoint(55,118,4000);
+    intake.move(127);
+    chassis.moveToPoint(55,118,4000,{},false);
+    chassis.moveDistance(10,1000,{.forwards=false},false);
+    
+
+    //66 127
+    chassis.turnToPoint(66,127,4000,{.forwards=false});
+    intake.move(0);
+    chassis.moveDistance(50,3000,{.forwards=false},false);
+    mogo.set_value(false);
+    pros::delay(500);
+    chassis.moveDistance(10,4000);
+
 
 }
