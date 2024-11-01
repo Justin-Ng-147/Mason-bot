d222322c8b171d0c22f497b92e75a41f911ae06a
Justin <justin.ng147@gmail.com>
2024-11-01 13:37:38 -0700

    red2 done

--- src/autons/red2.cpp
+++ src/autons/red2.cpp
@@ -20,23 +20,46 @@ void red2(){
     chassis.moveDistance(40,3000,{.forwards=false,.maxSpeed=60});
     swiper.set_value(false);
     while(!mogo_seated() && chassis.isInMotion()) pros::delay(10);
-    pros::delay(50);
+    pros::delay(100);
     mogo.set_value(true);
-    pros::delay(50);
+    pros::delay(100);
     chassis.cancelMotion();
 
     //20 36
     intake.move(127);
-    chassis.moveToPoint(20,32,2000,{},false);
+    chassis.moveToPoint(15,32,2000,{},false);
     // intake.move(0);
 
+    //start
+    // chassis.turnToPoint(5,-3,2000,{.forwards=false,.minSpeed = 10,.earlyExitRange=5});
+    // chassis.moveToPoint(5,-3,2000,{.forwards=false},false);
+    // chassis.turnToPoint(22,-9,2000,{},false);
+    // swiper.set_value(true);
+    // chassis.moveToPoint(22,-9,2000,{},false);
+    // intake.move(0);
+    // chassis.turnToHeading(35,3000,{.direction=AngularDirection::CCW_COUNTERCLOCKWISE, .minSpeed=127, .earlyExitRange = 3},false);
+    // left.move(127);
+    // right.move(-127);
+    // pros::delay(50);
+    // left.move(0);
+    // right.move(0);
+    // swiper.set_value(false);
+    // intake.move(127);
+    // chassis.moveDistance(12,2000,{.maxSpeed=30,.minSpeed=30,.earlyExitRange=3});
+    // chassis.turnToPoint(32,28,3000);
+    // chassis.moveDistance(40,10000,{.maxSpeed=30,.minSpeed=30,.earlyExitRange=3},false);
+    //end
 
-    chassis.turnToPoint(5,-3,2000,{.forwards=false,.minSpeed = 10,.earlyExitRange=5});
-    chassis.moveToPoint(5,-3,2000,{.forwards=false},false);
+
+
+
+    chassis.turnToPoint(-4,-5,2000,{.forwards=false,.minSpeed = 10,.earlyExitRange=5});
+    chassis.moveToPoint(-4,-5,2000,{.forwards=false},false);
+    chassis.turnToPoint(22,-12,2000,{},false);
     swiper.set_value(true);
-    chassis.turnToPoint(22,-9,2000,{});
-    chassis.moveToPoint(22,-9,2000);
-    chassis.turnToHeading(40,3000,{.direction=AngularDirection::CCW_COUNTERCLOCKWISE, .minSpeed=120, .earlyExitRange = 3},false);
+    chassis.moveToPoint(22,-12,2000,{},false);
+    intake.move(0);
+    chassis.turnToHeading(40,3000,{.direction=AngularDirection::CCW_COUNTERCLOCKWISE, .minSpeed=127, .earlyExitRange = 3},false);
     left.move(127);
     right.move(-127);
     pros::delay(50);
@@ -44,36 +67,30 @@ void red2(){
     right.move(0);
     swiper.set_value(false);
     intake.move(127);
-    // intake.move(127;)
-    // chassi
     chassis.moveDistance(12,2000,{.maxSpeed=30,.minSpeed=30,.earlyExitRange=3});
     chassis.turnToPoint(32,28,3000);
+    chassis.moveDistance(6,1000,{.forwards=false,.minSpeed=30,.earlyExitRange=3});
     chassis.moveDistance(40,10000,{.maxSpeed=30,.minSpeed=30,.earlyExitRange=3},false);
-    // chassis.waitUntil(20);
-    // chassis.cancelMotion();
-    // chassis.moveDistance(10,1000,{.forwards=false,.minSpeed = 30,.earlyExitRange=4});
-    // chassis.moveDistance(20,10000,{.maxSpeed=30});
-    // mogo.set_value(false);
 
+    
 
-
-    // chassis.moveDistance(15,2000,{.forwards=false},false);
-    // pros::delay(500);
-    // mogo.set_value(false);
-
-    // chassis.turnToPoint(28,45,2000,{.forwards=false});
-    // chassis.moveToPoint(28,45,2000,{.forwards = false,.maxSpeed=60});
-    // if(mogo_seated()){
-    //     pros::delay(50);
-    //     mogo.set_value(true);
-    //     pros::delay(50);
-    //     chassis.cancelMotion();
-    // }
-
+    chassis.moveDistance(15,2000,{.forwards=false},false);
     pros::delay(500);
     mogo.set_value(false);
-    chassis.turnToPoint(-23,39,2000,{.minSpeed=30,.earlyExitRange=3},false);
-    intake.move(0);
-    chassis.moveToPoint(-23,39,2000);
+
+    chassis.turnToPoint(20,40,2000,{.forwards=false});
+    chassis.moveToPoint(20,40,2000,{.forwards = false,.maxSpeed=60});
+    if(mogo_seated()){
+        pros::delay(50);
+        mogo.set_value(true);
+        pros::delay(50);
+        // chassis.cancelMotion();
+    }
+
+    // pros::delay(500);
+    // mogo.set_value(false);
+    // chassis.turnToPoint(-23,39,2000,{.minSpeed=30,.earlyExitRange=3},false);
+    // intake.move(0);
+    // chassis.moveToPoint(-23,39,2000);
 }
 
