ff77db31fb65b51de14473abb14aeaa7ab86099a
Justin <justin.ng147@gmail.com>
2025-02-12 14:18:45 -0800

    clean up red 5

--- src/autons/red5.cpp
+++ src/autons/red5.cpp
@@ -24,26 +24,12 @@ void red5(){
     mogo.set_value(false);
     chassis.cancelMotion();
 
-
-    // -18 11
-    //get 2 ring red
-    // chassis.turnToPoint(-18,11,2000,{},false);
-    // set_intake_speed(127);
-    // chassis.moveToPoint(-18,11,2000,{.maxSpeed=70});
-
     set_intake_speed(127);
-    // chassis.moveToPoint(-23,-5,2000,{.minSpeed=5,.earlyExitRange=40});
-    // chassis.moveToPoint(-23,-5,2000,{.maxSpeed=50,.minSpeed=5,.earlyExitRange=3},false);
     chassis.moveToPoint(-10,12,2000,{.minSpeed=5,.earlyExitRange=3},false);
     set_intake_speed(0);
-    // chassis.turnToPoint(39,28,1000,{.forwards=false,.minSpeed=20,.earlyExitRange=3});
-    // chassis.moveDistance(15,1000,{.forwards=false,.minSpeed=5,.earlyExitRange=3});
-    
 
     chassis.turnToPoint(45,29,1000,{.direction=lemlib::AngularDirection::CW_CLOCKWISE,.minSpeed=5,.earlyExitRange=3},false);
     set_intake_speed(127);
-    // mogo.set_value(false);
-    // set_intake_speed(127);
     chassis.moveToPoint(45,29,2000,{.minSpeed=5, .earlyExitRange=3});
     chassis.waitUntil(20);
     pros::Task red_pos_task{[=]
@@ -51,43 +37,15 @@ void red5(){
             while(distance.get_distance()>100) pros::delay(10);
             set_intake_speed(0);
         }};
-    // // fast_move(38,30,2000,false);
-    // chassis.moveDistance(10,1000,{.forwards=false},false);
-    // mogo.set_value(true);
     
-    // chassis.moveDistance(10,1000);
-    
-    
-    // chassis.turnToPoint(42,45.5,1000,{.forwards=false},false);
-    // chassis.moveToPoint(42,45.5,4000,{.forwards = false,.maxSpeed=70});
-    // while(!mogo_seated() && chassis.isInMotion()) pros::delay(10);
-    // pros::delay(50);
-    // mogo.set_value(false);
-    // chassis.cancelMotion();
 
-    // chassis.moveDistance(5,1000,{.minSpeed=127,.earlyExitRange=3});
-    // chassis.turnToPoint(38,-4,1000,{.minSpeed=5, .earlyExitRange=3},false);
-    // chassis.moveToPoint(38,-4,2000,{.minSpeed=5, .earlyExitRange=3});
-    // chassis.waitUntil(8);
-    // set_intake_speed(-127);
-    // chassis.moveDistance(7,1000,{.forwards=false,.minSpeed=80,.earlyExitRange=3});
-    // fast_move(38,0,2000,true);
     chassis.turnToPoint(47,-4,1000,{.minSpeed=5, .earlyExitRange=3},false);
     chassis.moveToPoint(47,-4,2000,{.minSpeed=5, .earlyExitRange=3});
-    // chassis.swingToHeading(135,lemlib::DriveSide::LEFT,1000,{.minSpeed=20,.earlyExitRange=10});
+
     chassis.turnToHeading(135,1000);
     set_intake_speed(127,false);
     chassis.moveDistance(40,1000,{.maxSpeed = 70});
     chassis.moveDistance(20,1000,{.forwards=false,.maxSpeed=60,.minSpeed=5,.earlyExitRange=3},false);
-    // intake_lift.set_value(true);
-    // chassis.moveDistance(8,1000,{.maxSpeed = 60});
-    // chassis.moveDistance(15,1000,{.forwards=false,.maxSpeed=60,.minSpeed=5,.earlyExitRange=3});
-    // intake_lift.set_value(false);
-    // chassis.moveDistance(10,1000,{.forwards=false,.minSpeed=80,.earlyExitRange=3},false);
-
-    // chassis.moveDistance(15,1000,{.forwards=false, .minSpeed=5, .earlyExitRange=3},false);
-    // pros::delay(1000);
-    // set_intake_speed(0);
     swiper.set_value(true);
     chassis.moveDistance(15,1000);
     pros::Task red_pos_task2{[=]
@@ -96,20 +54,15 @@ void red5(){
             pros::delay(500);
             set_intake_speed(0);
         }};
-    // chassis.moveDistance(10,500);
     chassis.turnToHeading(55,1000,{.minSpeed=5, .earlyExitRange=3},false);
     set_intake_speed(0);
     swiper.set_value(false);
-    // chassis.moveDistance(10,1000,{.minSpeed=5, .earlyExitRange=3});
-    // chassis.turnToHeading(55,1000,{.minSpeed=5, .earlyExitRange=3});
     chassis.turnToPoint(42,35,1000,{.minSpeed=5,.earlyExitRange=3});
     chassis.moveToPoint(42,35,2000,{.minSpeed=5, .earlyExitRange=3});
     
     chassis.waitUntil(10);
     mogo.set_value(true);
     chassis.turnToHeading(180,1000);
-    
-
 }
 
 
